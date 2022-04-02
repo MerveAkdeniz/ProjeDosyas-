87292250747f6b4048c365881621cd30885035bb
Merve Akdeniz <merve.akdeniz1@ogr.sakarya.edu.tr>
2022-04-02 11:00:25 +0300

    02.04.2022

--- yazilimprojesi/src/App.vue
+++ yazilimprojesi/src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" >
+    <Countries />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Countries from './components/Countries.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Countries,
   }
 }
 </script>
@@ -22,7 +21,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #133e69;
   margin-top: 60px;
 }
 </style>
---
+++ yazilimprojesi/src/components/Countries.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <div class="container">
+            <input type="text" class="search" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
+
+        </div>
+        <br><br>
+        <div class="container">
+            <table class="table table-hover table-dark table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">NAME</th>
+                        <th scope="col">CAPİTAL</th>
+                        <th scope="col">REGİON</th>
+                        <th scope="col">FLAG</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="country in countries">
+                        <td>{{country.name}}</td>
+                        <td>{{country.capital}}</td>
+                        <td>{{country.region}}</td>
+                        <td>{{country.flag}}</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr class="warning no-result" style="display:none;">
+                        <td colspan="8"><i class="fa fa-warning"></i> Bulunamadı</td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    export default {
+        data() {
+            return {
+                countries: null,
+            };
+        },
+        created: function () {
+            axios.get('https://restcountries.com/v2/all').then(res => {
+                this.countries = res.data;
+                console.log(this.countries);
+            })
+        }
+        
+    }
+</script>
+
+<style>
+
+</style>
